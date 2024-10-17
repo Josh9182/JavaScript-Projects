4d74c3f1301c4e82a9df01c83c0cb1fd0eb166c4
Josh Lewis <jlewis200413@gmail.com>
2024-10-17 11:55:06 -0600

    Update main.tsx

--- Automation Software UI/src/main.tsx
+++ Automation Software UI/src/main.tsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react'; // Component wrapper, provides code with extra alerts and checks for component fluidity.
 import { createRoot } from 'react-dom/client'; // Function, allows application to be optimized and run at a higher efficiency level
 import App from './App.tsx'; // App component which houses the linked Login component w/ Dashboard component
-import 'index.scss'; // index.css file for global customization
+import 'components/App.scss'; // App.scss file for global customization using SASS
+import 'components/Login.scss'; // Login.scss file for local login screen customization using SASS
+import 'components/Dashboard.scss'; // Login.scss file for local dashboard screen customization using SASS
+
 
 createRoot(document.getElementById("root")!).render( // App deployment utilizing createRoot, locating the div element with id = "root" from index.HTML and deploying inside the container.
   <StrictMode> 
