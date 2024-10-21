5082afdcf911ab99c884a2f064dc1850868ed5e7
Josh Lewis <jlewis200413@gmail.com>
2024-10-21 11:16:20 -0600

    Update Loading.tsx

--- Automation Software UI/src/components/Loading.tsx
+++ Automation Software UI/src/components/Loading.tsx
@@ -1,1 +1,24 @@
+import React from 'react';
+import './App.scss'
 
+const Loading = () => {
+
+  return (
+    <div id = "container">
+      <div id= "loading-symbol1">
+        <div id = "inner-symbol1">
+        </div>
+      </div>
+      <div id= "loading-symbol2">
+        <div id = "inner-symbol2">
+        </div>
+      </div>
+      <div id= "loading-symbol3">
+        <div id = "inner-symbol3">
+        </div>
+      </div>
+    </div>
+  )
+};
+
+export default Loading;
