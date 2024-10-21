4bbcb5486ed7168000e6dc78b6442ef3be8b6a19
Josh Lewis <jlewis200413@gmail.com>
2024-10-21 15:47:01 -0600

    Update main.tsx

--- Automation Software UI/src/main.tsx
+++ Automation Software UI/src/main.tsx
@@ -1,13 +1,19 @@
-import { StrictMode } from 'react'; // Component wrapper, provides code with extra alerts and checks for component fluidity.
-import { createRoot } from 'react-dom/client'; // Function, allows application to be optimized and run at a higher efficiency level
-import App from './App.tsx'; // App component which houses the linked Login component w/ Dashboard component
-import 'components/App.scss'; // App.scss file for global customization using SASS
-import 'components/Login.scss'; // Login.scss file for local login screen customization using SASS
-import 'components/Dashboard.scss'; // Login.scss file for local dashboard screen customization using SASS
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Login from './Login';
+import Dashboard from './Dashboard';
 
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="*" element={<Navigate to="/login" />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
 
-createRoot(document.getElementById("root")!).render( // App deployment utilizing createRoot, locating the div element with id = "root" from index.HTML and deploying inside the container.
-  <StrictMode> 
-    <App />
-  </StrictMode> // Surrounded with component wrapper StrictMode for security, formatted via JavaScript Prettier.
-);
+export default App;
