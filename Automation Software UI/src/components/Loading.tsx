546dfbb5e78d6f54ee3a2fb0c9f342728744599a
Josh Lewis <jlewis200413@gmail.com>
2024-10-21 11:17:15 -0600

    Update Loading.tsx

--- Automation Software UI/src/components/Loading.tsx
+++ Automation Software UI/src/components/Loading.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
-import './App.scss'
+import './App.scss';
 
 const Loading = () => {
-
   return (
-    <div id = "container">
-      <div id= "loading-symbol1">
-        <div id = "inner-symbol1">
-        </div>
+    <div id="container">
+      <div id="loading-symbol1">
+        <div id="inner-symbol1"></div>
       </div>
-      <div id= "loading-symbol2">
-        <div id = "inner-symbol2">
-        </div>
+      <div id="loading-symbol2">
+        <div id="inner-symbol2"></div>
       </div>
-      <div id= "loading-symbol3">
-        <div id = "inner-symbol3">
-        </div>
+      <div id="loading-symbol3">
+        <div id="inner-symbol3"></div>
       </div>
     </div>
-  )
+  );
 };
 
 export default Loading;
