65a42bb9469b19120f7adac692658e9b51ba7c55
Josh Lewis <jlewis200413@gmail.com>
2024-10-22 10:12:02 -0600

    Update App.tsx

--- Automation Software UI/src/App.tsx
+++ Automation Software UI/src/App.tsx
@@ -1,1 +1,21 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Loading from './Loading';
+import Login from './Login';
+import Dashboard from './Dashboard';
 
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/loading" element={<Loading />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="*" element={<Navigate to="/login" />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
