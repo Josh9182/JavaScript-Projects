6e7276dfa9be841b85bac828e9a0690c4e4fce79
Josh Lewis <jlewis200413@gmail.com>
2024-10-29 14:19:12 -0600

    Update Dashboard.tsx

--- Automation Software UI/src/components/Dashboard.tsx
+++ Automation Software UI/src/components/Dashboard.tsx
@@ -1,1 +1,50 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Login from './Login.tsx';
+
+const Dashboard = () => {
+  const [tableData, setTableData] = useState([]);
+  const [error, setError] = useState<string>('');
+  const navigate = useNavigate();
+
+  const FileUpload = (e) => {
+    const file = e.target.files[0];
+    const freader = new FileReader();
+
+    freader.onload = (e) => {
+      const data = e.target.result;
+
+      if (file.name.endsWith('.csv')) {
+        const rows = data.split('\n');
+        const cols = rows.map((row) => row.split(','));
+        setTableData(cols);
+      } else if (file.name.endsWith('.json')) {
+        try {
+          const jsonData = JSON.parse(data);
+          setTableData(jsonData);
+        } catch (err) {
+          setError('JSON file is invalid. Please resubmit.');
+        }
+      } else {
+        setError('File type invalid. Please resubmit.');
+      }
+    };
+    freader.readAsText(file);
+  };
+  return (
+    <div id="dashboard-container">
+      <div id="dashboard-banner">
+        <h1 id="dashboard-logo">AST</h1>
+      </div>
+      <div id="error-container">
+        <h1 id="error-element">{error}</h1>
+      </div>
+      <div id="input-container">
+        <input type="file" accept=".csv, .json" onChange={FileUpload} />
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
 
