8dd5e3122ae19026fdda6de49a851980833eb716
Josh Lewis <jlewis200413@gmail.com>
2024-10-31 14:23:45 -0600

    Update ProgressBar.tsx

--- Automation Software UI/src/components/ProgressBar.tsx
+++ Automation Software UI/src/components/ProgressBar.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import './ProgressBar.scss';
 
-const ProgressBar = () => {
+interface ProgressBarProps {
+  duration: number
+}
+
+const ProgressBar = ({ duration }: ProgressBarProps) => {
   return (
     <div id="pbar-container">
       <div id="pbar-outer">
-        <div id="pbar-inner"></div>
+        <div
+          id="pbar-inner"
+          style={{ animationDuration: `${duration}s` }}
+        ></div>
       </div>
     </div>
   );
