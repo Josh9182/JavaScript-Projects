fcd58be0931a9bd1136f94d036f2abde216f8fbb
Josh Lewis <jlewis200413@gmail.com>
2024-10-31 11:39:01 -0600

    Update ProgressBar.tsx

--- Automation Software UI/src/components/ProgressBar.tsx
+++ Automation Software UI/src/components/ProgressBar.tsx
@@ -1,1 +1,14 @@
+import React from 'react';
+import './ProgressBar.scss';
 
+const ProgressBar = () => {
+  return (
+    <div id="pbar-container">
+      <div id="pbar-outer">
+        <div id="pbar-inner"></div>
+      </div>
+    </div>
+  );
+};
+
+export default ProgressBar;
