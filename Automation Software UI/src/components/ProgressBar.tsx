b72be7c53c0368644c8ea47b423334c7c96a1cbd
Josh Lewis <jlewis200413@gmail.com>
2024-11-05 11:50:09 -0700

    Update ProgressBar.tsx

--- Automation Software UI/src/components/ProgressBar.tsx
+++ Automation Software UI/src/components/ProgressBar.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import './ProgressBar.scss';
 
-interface ProgressBarProps {
+interface ProgressBarProps { {/* Prop interface for a dynamic duration prop which will be filled in when imported into the Dashboard.tsx file. */}
   duration: number
 }
 
-const ProgressBar = ({ duration }: ProgressBarProps) => {
+const ProgressBar = ({ duration }: ProgressBarProps) => { {/* Component requiring and accessing the props above */}
   return (
     <div id="pbar-container">
       <div id="pbar-outer">
         <div
           id="pbar-inner"
-          style={{ animationDuration: `${duration}s` }}
+          style={{ animationDuration: `${duration}s` }} {/* Animation length set by dynamic duration prop */}
         ></div>
       </div>
     </div>
