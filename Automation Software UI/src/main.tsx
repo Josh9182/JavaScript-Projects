d55ae253594c62880cb0a5657c2c4eb6ba9aec56
Josh Lewis <jlewis200413@gmail.com>
2024-11-05 10:15:44 -0700

    Update main.tsx

--- Automation Software UI/src/main.tsx
+++ Automation Software UI/src/main.tsx
@@ -3,8 +3,14 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './App.scss'
 
+{/*
+Rendering App file inside of root div 
+surrounding by StrictMode for error checking and code safety
+*/}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
 );
+
