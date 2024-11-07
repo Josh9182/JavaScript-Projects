c919ac74632f3b8ca66e7a162d34cb1af82068d0
Josh Lewis <jlewis200413@gmail.com>
2024-11-07 14:54:01 -0700

    Update Login.tsx

--- Automation Software UI/src/components/Login.tsx
+++ Automation Software UI/src/components/Login.tsx
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import Loading from './Loading.tsx';
 import './Login.scss';
 
-const App = () => { 
+const App = () => {
   const [username, setUser] = useState<string>('');
   const [password, setPass] = useState<string>('');
   const [heading1, setHead1] = useState<string>('Sign In');
