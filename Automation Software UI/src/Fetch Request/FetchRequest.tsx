d4e8d7059213d2a6ab77e48e2fb1a474b3b350d4
Josh Lewis <jlewis200413@gmail.com>
2024-12-10 14:24:54 -0700

    Update FetchRequest.tsx

--- Automation Software UI/src/Fetch Request/FetchRequest.tsx
+++ Automation Software UI/src/Fetch Request/FetchRequest.tsx
@@ -2,26 +2,26 @@ export const fetchData = async (array_data) => {
   const URL = 'https://localhost:5001';
   const endpoint = `${URL}/api/data`;
 
-  const AC = new AbortController();
-  const timeout = setTimeout(() => {
+  const AC = new AbortController(); /* AbortController instance */
+  const timeout = setTimeout(() => { /* Initiate 7 second fail safe timer */
     AC.abort();
   }, 7000);
 
-  try {
+  try { /* "try" block to run fetch POST request */
     const response = await fetch(endpoint, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify(array_data),
-      signal: AC.signal,
+      signal: AC.signal, /* Link AbortController to try block */
     });
 
     if (!response.ok) {
       const errorMessage = await response.text();
       throw new Error(`Fetch request failed: ${errorMessage}`);
     } else {
-      return await response.json();
+      return await response.json(); /* Returns global response value for backend acceptance */
     }
   } catch (error) {
     if (error.name === 'AbortError') {
@@ -29,6 +29,6 @@ export const fetchData = async (array_data) => {
     }
     throw error;
   } finally {
-    clearTimeout(timeout);
+    clearTimeout(timeout); /* Clear timer to remove possible timer stacking i.e chaos */
   }
 };
